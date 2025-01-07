92a7bff0580f9490944e93a69088d92bd77998cf
aliensbright <144503232+aliensbright@users.noreply.github.com>
2025-01-07 11:28:41 -0800

    Done?

--- task1.py
+++ task1.py
@@ -3,19 +3,44 @@ Rectangular Prism Object
 Create a class that creates a rectangular prism.  You should be able to set all of the important measurements (l,w,h) when the object is instantiated in the constructor and you should have class methods that determine the surface area and volume.
 You should have class methods that also allow you to change the dimensions of the object.
 Instantiate 3 separate rectangular prisms with the test data given, and check the assertions are correct.
+    def calcDiameterToRadius(self):
+        self.radius = self.diameter / 2
+    
+    def calcRadiusToDiameter(self):
+        self.diameter = self.radius * 2
+
+    def setRadius(self,value):
+        self.radius = value
+        self.calcRadiusToDiameter()
 """
 
 class rectPrism:
+    length = 0
+    width = 0
+    height = 0
 
-    def __init__(self):
+    def __init__(self,l=0,w=0,h=0):
         # note you will need to specify more input parameters
-        pass
+        self.length = l
+        self.height = w
+        self.width = h
+        self.volume()
+        self.surfaceArea()
+
+
 
     def volume(self):
-        return
+        V = self.length * self.width * self.height
+        if V == 0:
+            V = None
+        return V
     
     def surfaceArea(self):
-        return
+        SA = self.length * self.width * 2 + self.width * self.height * 2 + self.length * self.height * 2
+        if self.length == 0 or self.width == 0 or self.height == 0:
+            SA = None
+        return SA
+
 
 # class instances and assertions below:
 
@@ -25,7 +50,7 @@ assert a.surfaceArea() == 160
 
 b = rectPrism(l=1,w=1,h=1)
 assert b.volume() == 1
-assert b.surfaceArea == 6
+assert b.surfaceArea() == 6
 
 c = rectPrism(l=2,w=0,h=10)
 # note the invalid width
--- task2.py
+++ task2.py
@@ -5,6 +5,31 @@ Create a class object that accepts paramters for Principal, Annual Interest Rate
 Create a class method that calculates the amount of compound interest for a given length of time.
 
 Extension: accept time given in different measurements, but convert them to years for use in your class template.
+    length = 0
+    width = 0
+    height = 0
+
+    def __init__(self,l=0,w=0,h=0):
+        # note you will need to specify more input parameters
+            self.length = l
+            self.height = w
+            self.width = h
+            self.volume()
+            self.surfaceArea()
+
+
+    def volume(self):
+        V = self.length * self.width * self.height
+        if V == 0:
+            V = None
+        return V
+    
+    def surfaceArea(self):
+        SA = self.length * self.width * 2 + self.width * self.height * 2 + self.length * self.height * 2
+        if self.length == 0 or self.width == 0 or self.height == 0:
+            SA = None
+        return SA
+
 """
 
 
@@ -13,15 +38,25 @@ class Calc:
     rate = 0
     nPeriods = 0
 
-    def __init__(self):
+    def __init__(self,P=0,r=0,n=0,t=0):
+        self.principal = P
+        self.rate = r / 100
+        self.nPeriods = n
+        self.interest(t)
+        self.amount(t)
         #more input parameters needed
         return
 
     def interest(self,t):
-        return 
+        Amt = self.principal * (1 + self.rate / self.nPeriods) ** (self.nPeriods * t)
+        Int = Amt - self.principal
+        Int = round(Int,2)
+        return Int
     
     def amount(self,t):
-        return
+        Amt = self.principal * (1 + self.rate / self.nPeriods) ** (self.nPeriods * t)
+        Amt = round(Amt,2)
+        return Amt
 
 a = Calc(P=1000,r=4,n=2)
 assert a.interest(3) == 126.16
